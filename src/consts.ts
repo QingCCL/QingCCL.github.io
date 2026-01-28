fb6c48d178103a76edb91dcddc31f9172179b25d
gwhg45yh5ehsergfdxfgh4wuy56jhndgf <ardgthg564y@435t4.gth>
2026-01-28 16:13:21 +0800

    首次提交

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'QingCCL'
+export const SITE_DESCRIPTION = 'QingCCL的个人站点，分享日常随笔、心得感悟、所思所想'
