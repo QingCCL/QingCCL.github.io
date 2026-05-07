9254e773f530d0e791892d6f3993648faa2a0678
QingCCL <sakznb666@gmail.com>
2026-05-07 03:25:44 +0000

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# QingCCL.github.io
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'QingCCL'
+export const SITE_DESCRIPTION = 'QingCCL的个人站点，分享日常随笔、心得感悟、所思所想、读书、文学、读后感、编程'
