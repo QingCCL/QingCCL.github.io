95ec2e1df58e2b2a6eb8c59c556d7c63b49b9585
QingCCL <sakznb666@gmail.com>
2026-07-21 13:38:14 +0800

    更新

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
   darkMode: "class", // allows toggling dark mode manually
   theme: {
     extend: {
-      fontSize:{
-        'base': ['1.0625rem', { // 1rem 通常等于 16px
-          lineHeight: '1.75',
-        }],
-      },
       fontFamily: {
-        sans: [ "sans-serif"],
+        sans: ["sans-serif"],
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
 }
